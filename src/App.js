8834108a566fefc23a3f63ca6629c764671171a2
Mannex Pimentel <Mannex@mannexs-air.attlocal.net>
2019-04-22 23:46:14 -0400

    running night check

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Index from './components/layout/Index'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import Lyrics from './components/tracks/Lyrics';
+
 import { Provider }  from './context';
 import './App.css';
 
@@ -14,9 +16,11 @@ class App extends Component {
         <Router>
           <React.Fragment>
             <Navbar />
-            <div className="container">
+            <div className = "container">
               <Switch>
                 <Route exact path="/" component={ Index } />
+                <Route exact path="/lyrics/track/:id" component={ Lyrics } />
+                
               </Switch>
             </div>
           </React.Fragment>
---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Lyrics extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Lyrics</h1>
+    </React.Fragment>
+    );
+  }
+}
+
+export default Lyrics;
