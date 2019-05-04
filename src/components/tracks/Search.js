a6f6a35c86f085d52c41d1d61bddb87a48beed33
Mannex Pimentel <mannex247@gmail.com>
2019-05-04 01:11:52 -0400

    axios search function

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -1,45 +1,65 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Consumer } from '../../context'
-
+import React, { Component } from "react";
+import axios from "axios";
+import { Consumer } from "../../context";
 
 class Search extends Component {
-    state = {
-        trackTitle:''
-    };
+  state = {
+    trackTitle: ""
+  };
 
-    onChange = e => {
-        this.setState({ [e.target.name]: e.target.value});
-    }
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    render() {
-        return (
-           <Consumer>
-               {value => {
-                return (
-                    <div className="card card-body mb-4 p-4">
-                    <h1 className="display-4 text-center">
-                        <i className="fas fa-music"></i>Search For A Song
-                    </h1>
-                        <p className="lead text-center">Get The Lyrice For Any Song</p>
-                        <form>
-                            <div className="form-group">
-                                <input 
-                                type="text" 
-                                className="form-control form-control-lg" 
-                                placeholder="Song title..."
-                                name="trackTitle"
-                                value={this.state.trackTitle}
-                                onChange={this.onChange}
-                                />
-                            </div>
-                        </form>
-                    </div>
-                );
-               }}
-           </Consumer>
-        );
-    }
+  findTrack = (e) => {
+      e.preventDefault();
+      axios
+    .get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}
+`)
+    .then(res => {
+
+        console.log(res.data)
+      
+    })
+    .catch(err => console.log(err));
+
+  }
+
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          return (
+            <div className="card card-body mb-4 p-4">
+              <h1 className="display-4 text-center">
+                <i className="fas fa-music" />Search For A Song
+              </h1>
+              <p className="lead text-center">Get The Lyrice For Any Song</p>
+              <form onSubmit={this.findTrack}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Song title..."
+                    name="trackTitle"
+                    value={this.state.trackTitle}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <button
+                  className="btn btn-primary btn-lg btn-block mb-5"
+                  type="submit"
+                >
+                  {" "}
+                  Get Track Lyrics
+                </button>
+              </form>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
 }
 
 export default Search;
